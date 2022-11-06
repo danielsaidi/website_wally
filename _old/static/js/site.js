fb1733b001a36ea75fc451607b6d7c72fe76b802
Daniel Saidi <daniel.saidi@gmail.com>
2022-11-06 20:37:02 +0100

    Move reusable include files to shared repo

--- _old/static/js/site.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("a[rel='external']").attr("target", "_blank");
-  
-  $(function () {
-  	$("a.youtube").YouTubePopup({ autoplay: 1, hideTitleBar: 1 });
-  });
-});
